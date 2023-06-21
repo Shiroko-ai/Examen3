854c563a516556c4aed1e08f8857cba6b4c5fa07
ivanpalacios2809@gmail.com <ivanpalacios2809@gmail.com>
2023-06-20 22:56:46 -0600

    ya esta el asignar grupos

--- public/dist/js-propios/create-groups.js
+++ public/dist/js-propios/create-groups.js
@@ -14,3 +14,25 @@ function createGroup(){
       });
       
 }
+function createUser(){
+    let groupName  = document.getElementById('groupName').value;
+    let user  = document.getElementById('user').value;
+    let TAG  = document.getElementById('TAG').value;
+    let password  = document.getElementById('password').value;
+    console.log(groupName, user, TAG, password);
+    $.ajax({
+        method:"POST",
+        url: "http://localhost:3000/group/set-user",
+        data: { 
+            TAG: TAG,
+            user: user,
+            password: password,
+            group: groupName
+        },
+        success: function( result ) {
+          alert(result);
+          location.reload();
+        }
+      });
+      
+}
